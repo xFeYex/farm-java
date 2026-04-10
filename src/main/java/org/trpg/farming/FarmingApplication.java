f075ec29504e9db4f93a937941b53bfe8da86277
itFeYex <2810075281@qq.com>
2026-04-10 14:18:09 +0800

    init

---
+++ src/main/java/org/trpg/farming/FarmingApplication.java
@@ -0,0 +1,13 @@
+package org.trpg.farming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmingApplication.class, args);
+    }
+
+}
